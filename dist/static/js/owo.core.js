3d1f560ee49a8e9a47f97ebc5c3d327ef329e1ac
puge <mail@puge.org>
2020-07-28 11:12:12 +0800

    水

--- dist/static/js/owo.core.js
+++ dist/static/js/owo.core.js
@@ -1,4 +1,4 @@
-// Mon Jul 27 2020 23:08:46 GMT+0800 (GMT+08:00)
+// Tue Jul 28 2020 11:10:47 GMT+0800 (GMT+08:00)
 var owo = {tool: {},state: {},};
 /* 方法合集 */
 var _owo = {
--- index.js
+++ index.js
@@ -9,445 +9,65 @@ var infoData = [
     top: 633,
     index: 26
   },
-  {
-    name: '文字-村',
-    fileName: '@|文字-村.png|',
-    width: 84,
-    height: 84,
-    opacity: 100,
-    left: 5680,
-    top: 712,
-    index: 28
-  },
-  {
-    name: '文字-山',
-    fileName: '@|文字-山.png|',
-    width: 85,
-    height: 85,
-    opacity: 100,
-    left: 4558,
-    top: 741,
-    index: 30
-  },
-  {
-    name: '文字-瓷',
-    fileName: '@|文字-瓷.png|',
-    width: 84,
-    height: 84,
-    opacity: 100,
-    left: 6822,
-    top: 582,
-    index: 32
-  },
-  {
-    name: '文字-路',
-    fileName: '@|文字-路.png|',
-    width: 85,
-    height: 84,
-    opacity: 100,
-    left: 13226,
-    top: 568,
-    index: 34
-  },
-  {
-    name: '文字-茶',
-    fileName: '@|文字-茶.png|',
-    width: 87,
-    height: 87,
-    opacity: 100,
-    left: 11054,
-    top: 931,
-    index: 36
-  },
-  {
-    name: '文字-土',
-    fileName: '@|文字-土.png|',
-    width: 88,
-    height: 88,
-    opacity: 100,
-    left: 12370,
-    top: 634,
-    index: 38
-  },
-  {
-    name: '文字-药',
-    fileName: '@|文字-药.png|',
-    width: 88,
-    height: 87,
-    opacity: 100,
-    left: 11491,
-    top: 551,
-    index: 40
-  },
-  {
-    name: '文字-橙',
-    fileName: '@|文字-橙.png|',
-    width: 85,
-    height: 85,
-    opacity: 100,
-    left: 7746,
-    top: 685,
-    index: 42
-  },
-  {
-    name: '路山村-房子-1',
-    fileName: '@|路山村-房子-1.png|',
-    width: 351,
-    height: 241,
-    opacity: 100,
-    left: 6094,
-    top: 782,
-    index: 44
-  },
-  {
-    name: '路山村-房子-2',
-    fileName: '@|路山村-房子-2.png|',
-    width: 715,
-    height: 250,
-    opacity: 100,
-    left: 5477,
-    top: 779,
-    index: 46
-  },
-  {
-    name: '路山村-房子-3',
-    fileName: '@|路山村-房子-3.png|',
-    width: 311,
-    height: 232,
-    opacity: 100,
-    left: 6329,
-    top: 773,
-    index: 48
-  },
-  {
-    name: '路山村-房子-4',
-    fileName: '@|路山村-房子-4.png|',
-    width: 407,
-    height: 384,
-    opacity: 100,
-    left: 6295,
-    top: 640,
-    index: 50
-  },
-  {
-    name: '路山村-大树',
-    fileName: '@|路山村-大树.png|',
-    width: 455,
-    height: 428,
-    opacity: 100,
-    left: 5994,
-    top: 568,
-    index: 52
-  },
-  {
-    name: '路山村-房子-5',
-    fileName: '@|路山村-房子-5.png|',
-    width: 477,
-    height: 325,
-    opacity: 100,
-    left: 6560,
-    top: 637,
-    index: 54
-  },
-  {
-    name: '路山村-瓷器-1',
-    fileName: '@|路山村-瓷器-1.png|',
-    width: 39,
-    height: 66,
-    opacity: 100,
-    left: 7154,
-    top: 796,
-    index: 56
-  },
-  {
-    name: '路山村-瓷器-2',
-    fileName: '@|路山村-瓷器-2.png|',
-    width: 54,
-    height: 100,
-    opacity: 100,
-    left: 7107,
-    top: 764,
-    index: 58
-  },
-  {
-    name: '路山村-房子-6',
-    fileName: '@|路山村-房子-6.png|',
-    width: 272,
-    height: 255,
-    opacity: 100,
-    left: 6899,
-    top: 604,
-    index: 60
-  },
-  {
-    name: '路山村-房子-7',
-    fileName: '@|路山村-房子-7.png|',
-    width: 383,
-    height: 157,
-    opacity: 100,
-    left: 6797,
-    top: 668,
-    index: 62
-  },
-  {
-    name: '路山村-房子-8',
-    fileName: '@|路山村-房子-8.png|',
-    width: 357,
-    height: 188,
-    opacity: 100,
-    left: 6327,
-    top: 563,
-    index: 64
-  },
-  {
-    name: '路山村-房子-9',
-    fileName: '@|路山村-房子-9.png|',
-    width: 392,
-    height: 177,
-    opacity: 100,
-    left: 6677,
-    top: 593,
-    index: 66
-  },
-  {
-    name: '路山村-井冈山装饰',
-    fileName: '@|路山村-井冈山装饰.png|',
-    width: 377,
-    height: 206,
-    opacity: 100,
-    left: 4783,
-    top: 757,
-    index: 68
-  },
-  {
-    name: '路山村-竹子',
-    fileName: '@|路山村-竹子.png|',
-    width: 165,
-    height: 120,
-    opacity: 100,
-    left: 4652,
-    top: 810,
-    index: 70
-  },
-  {
-    name: '路山村-大树-2',
-    fileName: '@|路山村-大树-2.png|',
-    width: 302,
-    height: 234,
-    opacity: 100,
-    left: 5305,
-    top: 730,
-    index: 72
-  },
-  {
-    name: '路山村-背景',
-    fileName: '@|路山村-背景.png|',
-    width: 5135,
-    height: 674,
-    opacity: 100,
-    left: 3082,
-    top: 649,
-    index: 74
-  },
-  {
-    name: '路山村-大山-2',
-    fileName: '@|路山村-大山-2.png|',
-    width: 799,
-    height: 499,
-    opacity: 100,
-    left: 4294,
-    top: 439,
-    index: 76
-  },
-  {
-    name: '路山村-大山',
-    fileName: '@|路山村-大山.png|',
-    width: 949,
-    height: 557,
-    opacity: 100,
-    left: 4956,
-    top: 410,
-    index: 78
-  },
-  {
-    name: '路山村-竹子-2',
-    fileName: '@|路山村-竹子-2.png|',
-    width: 229,
-    height: 327,
-    opacity: 100,
-    left: 4908,
-    top: 540,
-    index: 80
-  },
-  {
-    name: '路山村-房子-10',
-    fileName: '@|路山村-房子-10.png|',
-    width: 601,
-    height: 305,
-    opacity: 100,
-    left: 5660,
-    top: 535,
-    index: 82
-  },
-  {
-    name: '路山村-房子-11',
-    fileName: '@|路山村-房子-11.png|',
-    width: 372,
-    height: 158,
-    opacity: 100,
-    left: 6099,
-    top: 489,
-    index: 84
-  },
-  {
-    name: '路山村-云',
-    fileName: '@|路山村-云.png|',
-    width: 851,
-    height: 372,
-    opacity: 100,
-    left: 4670,
-    top: 421,
-    index: 86
-  },
-  {
-    name: '路山村-大山-3',
-    fileName: '@|路山村-大山-3.png|',
-    width: 868,
-    height: 601,
-    opacity: 100,
-    left: 4607,
-    top: 261,
-    index: 88
-  },
-  {
-    name: '路山村-大山-4',
-    fileName: '@|路山村-大山-4.png|',
-    width: 1097,
-    height: 515,
-    opacity: 100,
-    left: 3715,
-    top: 289,
-    index: 90
-  },
-  {
-    name: '路山村-大山-5',
-    fileName: '@|路山村-大山-5.png|',
-    width: 1071,
-    height: 445,
-    opacity: 100,
-    left: 5175,
-    top: 278,
-    index: 92
-  },
-  {
-    name: '远山',
-    fileName: '@|远山.png|',
-    width: 5094,
-    height: 779,
-    opacity: 100,
-    left: 2990,
-    top: 46,
-    index: 94
-  },
   // {
-  //   name: '橙-橙子',
-  //   fileName: '@|橙-橙子.png|',
-  //   width: 196,
-  //   height: 178,
+  //   name: '文字-村',
+  //   fileName: '@|文字-村.png|',
+  //   width: 84,
+  //   height: 84,
   //   opacity: 100,
-  //   left: 8399,
-  //   top: 985,
-  //   index: 96
+  //   left: 5680,
+  //   top: 712,
+  //   index: 28
   // },
   // {
-  //   name: '橙-橙树-1',
-  //   fileName: '@|橙-橙树-1.png|',
-  //   width: 565,
-  //   height: 378,
+  //   name: '文字-山',
+  //   fileName: '@|文字-山.png|',
+  //   width: 85,
+  //   height: 85,
   //   opacity: 100,
-  //   left: 7931,
-  //   top: 725,
-  //   index: 98
+  //   left: 4558,
+  //   top: 741,
+  //   index: 30
   // },
   // {
-  //   name: '橙-房子-1',
-  //   fileName: '@|橙-房子-1.png|',
-  //   width: 427,
-  //   height: 327,
+  //   name: '文字-路',
+  //   fileName: '@|文字-路.png|',
+  //   width: 85,
+  //   height: 84,
   //   opacity: 100,
-  //   left: 8292,
-  //   top: 725,
-  //   index: 100
+  //   left: 13226,
+  //   top: 568,
+  //   index: 34
   // },
   // {
-  //   name: '橙-石头',
-  //   fileName: '@|橙-石头.png|',
-  //   width: 1421,
-  //   height: 472,
+  //   name: '文字-茶',
+  //   fileName: '@|文字-茶.png|',
+  //   width: 87,
+  //   height: 87,
   //   opacity: 100,
-  //   left: 7386,
-  //   top: 828,
-  //   index: 102
+  //   left: 11054,
+  //   top: 931,
+  //   index: 36
   // },
   // {
-  //   name: '橙-橙树-2',
-  //   fileName: '@|橙-橙树-2.png|',
-  //   width: 359,
-  //   height: 368,
+  //   name: '文字-土',
+  //   fileName: '@|文字-土.png|',
+  //   width: 88,
+  //   height: 88,
   //   opacity: 100,
-  //   left: 7737,
-  //   top: 767,
-  //   index: 104
+  //   left: 12370,
+  //   top: 634,
+  //   index: 38
   // },
   // {
-  //   name: '橙-橙树-3',
-  //   fileName: '@|橙-橙树-3.png|',
-  //   width: 379,
-  //   height: 310,
+  //   name: '文字-药',
+  //   fileName: '@|文字-药.png|',
+  //   width: 88,
+  //   height: 87,
   //   opacity: 100,
-  //   left: 8564,
-  //   top: 734,
-  //   index: 106
-  // },
-  // {
-  //   name: '橙-橙树-4',
-  //   fileName: '@|橙-橙树-4.png|',
-  //   width: 399,
-  //   height: 281,
-  //   opacity: 100,
-  //   left: 8855,
-  //   top: 1008,
-  //   index: 108
-  // },
-  // {
-  //   name: '橙-路面',
-  //   fileName: '@|橙-路面.png|',
-  //   width: 1628,
-  //   height: 330,
-  //   opacity: 100,
-  //   left: 7682,
-  //   top: 957,
-  //   index: 110
-  // },
-  // {
-  //   name: '橙-树',
-  //   fileName: '@|橙-树.png|',
-  //   width: 501,
-  //   height: 385,
-  //   opacity: 100,
-  //   left: 7875,
-  //   top: 660,
-  //   index: 112
-  // },
-  // {
-  //   name: '橙-江水',
-  //   fileName: '@|橙-江水.png|',
-  //   width: 589,
-  //   height: 263,
-  //   opacity: 100,
-  //   left: 7553,
-  //   top: 682,
-  //   index: 114
+  //   left: 11491,
+  //   top: 551,
+  //   index: 40
   // },
   // {
   //   name: '茶',
@@ -459,96 +79,106 @@ var infoData = [
   //   top: 914,
   //   index: 116
   // },
+  {
+    name: '水-小舟',
+    fileName: '@|水-小舟.png|',
+    width: 115,
+    height: 123,
+    opacity: 100,
+    left: 10412,
+    top: 759,
+    index: 118
+  },
+  {
+    name: '水-江水',
+    fileName: '@|水-江水.png|',
+    width: 662,
+    height: 123,
+    opacity: 100,
+    left: 8488,
+    top: 670,
+    index: 120
+  },
+  {
+    name: '水-江水-2',
+    fileName: '@|水-江水-2.png|',
+    width: 595,
+    height: 85,
+    opacity: 100,
+    left: 10258,
+    top: 767,
+    index: 122
+  },
+  {
+    name: '水-江水-3',
+    fileName: '@|水-江水-3.png|',
+    width: 1062,
+    height: 123,
+    opacity: 100,
+    left: 9808,
+    top: 880,
+    index: 124
+  },
+  {
+    name: '水-江水-4',
+    fileName: '@|水-江水-4.png|',
+    width: 1171,
+    height: 265,
+    opacity: 100,
+    left: 9292,
+    top: 988,
+    index: 126
+  },
+  {
+    name: '水-江水-5',
+    fileName: '@|水-江水-5.png|',
+    width: 557,
+    height: 102,
+    opacity: 100,
+    left: 8953,
+    top: 930,
+    index: 128
+  },
+  {
+    name: '水-渔网',
+    fileName: '@|水-渔网.png|',
+    width: 373,
+    height: 215,
+    opacity: 100,
+    left: 8956,
+    top: 718,
+    index: 130
+  },
+  {
+    name: '水-鸟',
+    fileName: '@|水-鸟.png|',
+    width: 51,
+    height: 36,
+    opacity: 100,
+    left: 9859,
+    top: 696,
+    index: 132
+  },
+  {
+    name: '水-宝塔',
+    fileName: '@|水-宝塔.png|',
+    width: 1063,
+    height: 380,
+    opacity: 100,
+    left: 9259,
+    top: 599,
+    index: 134
+  },
   // {
-  //   name: '水-小舟',
-  //   fileName: '@|水-小舟.png|',
-  //   width: 115,
-  //   height: 123,
+  //   name: '远山-3',
+  //   fileName: '@|远山-3.png|',
+  //   width: 2958,
+  //   height: 677,
   //   opacity: 100,
-  //   left: 10412,
-  //   top: 759,
-  //   index: 118
-  // },
-  // {
-  //   name: '水-江水',
-  //   fileName: '@|水-江水.png|',
-  //   width: 662,
-  //   height: 123,
-  //   opacity: 100,
-  //   left: 8488,
-  //   top: 670,
-  //   index: 120
-  // },
-  // {
-  //   name: '水-江水-2',
-  //   fileName: '@|水-江水-2.png|',
-  //   width: 595,
-  //   height: 85,
-  //   opacity: 100,
-  //   left: 10258,
-  //   top: 767,
-  //   index: 122
-  // },
-  // {
-  //   name: '水-江水-3',
-  //   fileName: '@|水-江水-3.png|',
-  //   width: 1062,
-  //   height: 123,
-  //   opacity: 100,
-  //   left: 9808,
-  //   top: 880,
-  //   index: 124
-  // },
-  // {
-  //   name: '水-江水-4',
-  //   fileName: '@|水-江水-4.png|',
-  //   width: 1171,
-  //   height: 265,
-  //   opacity: 100,
-  //   left: 9292,
-  //   top: 988,
-  //   index: 126
-  // },
-  // {
-  //   name: '水-江水-5',
-  //   fileName: '@|水-江水-5.png|',
-  //   width: 557,
-  //   height: 102,
-  //   opacity: 100,
-  //   left: 8953,
-  //   top: 930,
-  //   index: 128
-  // },
-  // {
-  //   name: '水-渔网',
-  //   fileName: '@|水-渔网.png|',
-  //   width: 373,
-  //   height: 215,
-  //   opacity: 100,
-  //   left: 8956,
-  //   top: 718,
-  //   index: 130
-  // },
-  // {
-  //   name: '水-鸟',
-  //   fileName: '@|水-鸟.png|',
-  //   width: 51,
-  //   height: 36,
-  //   opacity: 100,
-  //   left: 9859,
-  //   top: 696,
-  //   index: 132
-  // },
-  // {
-  //   name: '水-宝塔',
-  //   fileName: '@|水-宝塔.png|',
-  //   width: 1063,
-  //   height: 380,
-  //   opacity: 100,
-  //   left: 9259,
-  //   top: 599,
-  //   index: 134
-  // },
+  //   left: 8031,
+  //   top: 615,
+  //   index: 158
+  // }
   // {
   //   name: '药土路-土块',
   //   fileName: '@|药土路-土块.png|',
@@ -659,22 +289,12 @@ var infoData = [
   //   top: 183,
   //   index: 156
   // },
-  // {
-  //   name: '远山-3',
-  //   fileName: '@|远山-3.png|',
-  //   width: 2958,
-  //   height: 677,
-  //   opacity: 100,
-  //   left: 8031,
-  //   top: 615,
-  //   index: 158
-  // }
 ]
 let newData = []
 let index = 0
 let data = ''
 infoData.forEach(element => {
-  data += `{name: '${element.name}',fileName: '${element.fileName}',width: ${element.width},height: ${element.height},opacity: ${element.opacity},left: ${element.left - 4000},top: ${element.top},index: ${element.index + 60}},
+  data += `{name: '${element.name}',fileName: '${element.fileName}',width: ${element.width},height: ${element.height},opacity: ${element.opacity},left: ${element.left - 9000},top: ${element.top - 800},index: ${element.index + 310}},
   `
 })
 
